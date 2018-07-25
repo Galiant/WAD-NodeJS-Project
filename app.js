a725522f4161b3f001fdbe97ae523a2740c16f9e
Galiant <antonijodev@gmail.com>
2018-07-25 23:16:13 +0000

    Added index page to application

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require("express"); // call express to be used by the application
 var app = express();
+const path = require('path');
+const VIEWS = path.join(__dirname, 'views');
 
 // Function to set up a simple hello response
 app.get('/', function(req,res) {
-    res.send("<h1>Hello World</h1>");
-    console.log("Hello");
+    // res.send("<h1>Hello World</h1>"); // This is commented out to allow the index view to be rendered
+    res.sendFile('index.html', {root: VIEWS});
+    console.log("Now you are home!");
 });
 
 // Set the requirements for application to run
-app.listen(process.env.port || 3000, process.env.IP || "0.0.0.0" , function() {
+app.listen(process.env.port || 8080, process.env.IP || "0.0.0.0" , function() {
     console.log("App is running ......... Yessssssssss!");
 });
