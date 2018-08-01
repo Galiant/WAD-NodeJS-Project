299e1519133d06ad9a5ef9961652b5bd5850a77c
Galiant <antonijodev@gmail.com>
2018-08-01 22:46:53 +0000

    Added sample styles what will be changed later on and added images and scripts folder and allow them in app,js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 const path = require('path');
 const VIEWS = path.join(__dirname, 'views');
 
+app.use(express.static("scripts"));  // allow the application to access the scripts folder contents to use in the application
+app.use(express.static("images"));  // allow the application to access the images folder contents to use in the application
+
 // Function to set up a simple hello response
 app.get('/', function(req,res) {
     // res.send("<h1>Hello World</h1>"); // This is commented out to allow the index view to be rendered
