0d49f84801ad7178e0485c4be36d1155a7951fbc
Galiant <antonijodev@gmail.com>
2018-08-02 21:41:14 +0000

    Implemented pug templating engine and created products page

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const VIEWS = path.join(__dirname, 'views');
 app.use(express.static("scripts"));  // allow the application to access the scripts folder contents to use in the application
 app.use(express.static("images"));  // allow the application to access the images folder contents to use in the application
 
-// Function to set up a simple hello response
+// Function to render the home page
 app.get('/', function(req,res) {
     // res.send("<h1>Hello World</h1>"); // This is commented out to allow the index view to be rendered
-    res.sendFile('index.html', {root: VIEWS});
-    console.log("Now you are home!");
+    res.render('index.pug', {root: VIEWS});
+    console.log("Now you are on the home page!");
+});
+
+// Function to render the products page
+app.get('/products', function(req,res) {
+    // res.send("<h1>Hello World</h1>"); // This is commented out to allow the index view to be rendered
+    res.render('products.pug', {root: VIEWS}); // use the render command so that response object renders a HTML page
+    console.log("Now you are on the products page!");
 });
 
 // Set the requirements for application to run
