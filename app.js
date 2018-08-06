5b00d29d0f3e9b820c14ed21d80f2551b0915e40
Galiant <antonijodev@gmail.com>
2018-08-06 19:34:34 +0000

    Created table and fill up table with product

--- app.js
+++ app.js
@@ -17,10 +17,42 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if(err) {
-        console.log("Database isn't connected!!!!")
+        console.log("Database isn't connected........Please check login details.......");
+        // throw(err)
     } else {
-        console.log("Well done. You are connected to database......")
+        console.log("Well done. You are connected to database......");
     }
+});
+
+// Create a database table
+app.get('/createtable', function(req, res) {
+    let sql = 'CREATE TABLE products (Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, Name varchar(255), Brand varchar(255), Category varchar(255), Price int, Image varchar(255));';
+    
+    let query = db.query(sql, (err, res) => {
+        if(err) throw err;
+        console.log(res);
+    });
+   res.send("Table changed........."); 
+});
+
+// SQL Insert Data Example
+app.get('/insert', function(req, res) {
+    let sql = 'INSERT INTO products (Name, Brand, Category, Price, Image) VALUES ("Baby Elegance Venti Travel System - Navy", "Baby Elegance", "Travel System", 449.99, "venti.jpg");';
+    let query = db.query(sql, (err, res) => {
+        if(err) throw err;
+        console.log(res);
+    });
+   res.send("Product created.........Thank you........");
+});
+
+// SQL Query just for show Example
+app.get('/queryme', function(req, res) {
+    let sql = 'SELECT * FROM products';
+    let query = db.query(sql, (err, res) => {
+        if(err) throw err;
+        console.log(res);
+    });
+   res.send("Look in the console........");
 });
 
 // Function to render the home page
@@ -33,7 +65,13 @@ app.get('/', function(req,res) {
 // Function to render the products page
 app.get('/products', function(req,res) {
     // res.send("<h1>Hello World</h1>"); // This is commented out to allow the index view to be rendered
-    res.render('products', {root: VIEWS}); // use the render command so that response object renders a HTML page
+    let sql = 'SELECT * FROM products;';
+    let query = db.query(sql, (err, res1) => {
+        if(err)
+        throw(err);
+    
+    res.render('products', {root: VIEWS, res1}); // use the render command so that response object renders a HTML page
+    });
     console.log("Now you are on the products page!");
 });
 
